6cdfb8c0a7bbe7f5f346773bf9e83d51be361c61
Ben Chaplin <ben.chaplin@gravitysupplychain.com>
2016-07-17 20:10:13 +0100

    XML sitemap stage 1.

---
+++ app/controllers/sitemap/sitemap.controller.js
@@ -0,0 +1,113 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var xml = require('xml');
+
+exports.list = function (req, res) {
+  var sitemapUrl = req.query.url,
+      sitemap = {},
+      noRequests = 0,
+      limit = 10,
+      alternates = {},
+      canonical = '';
+
+  function getSitemap(url) {
+    noRequests += 1;
+    if (noRequests < limit) {
+      request({url: url}, callback);
+    } else {
+      var xmlBody = [{
+        urlset: [
+          {
+            _attr: {
+              xmlns: 'http://www.sitemaps.org/schemas/sitemap/0.9',
+              'xmlns:image': 'http://www.google.com/schemas/sitemap-image/1.1'
+            }
+          }]
+      }];
+
+      for (var link in sitemap) {
+        var siteMapItem = [
+          {loc: link}
+        ];
+
+        for (var asset in sitemap[link].assets) {
+          siteMapItem.push({'image:image': [{'image:loc': sitemap[link].assets[asset]}]});
+        }
+
+        xmlBody[0].urlset.push({
+          url: siteMapItem
+        })
+      }
+
+      res.set('Content-Type', 'text/xml');
+      res.send(xml(xmlBody, true));
+    }
+  }
+
+  function requestNextLink() {
+    for (var link in sitemap) {
+      if (sitemap[link].visited !== true) {
+        sitemap[link].visited = true;
+        getSitemap(link);
+
+        break;
+      }
+    }
+
+    if (!request) {
+      res.json(sitemap);
+    }
+  }
+
+  function getLinks(tags, canonical, alt) {
+    for (var i = 0; i < tags.length; i += 1) {
+      var link = tags[i].attribs.href;
+
+      if (link && link.indexOf('/cdn-cgi') === -1 && link.indexOf('#') !== 0 && !alt[sitemapUrl + link] && link.indexOf('http') === -1) {
+        link = canonical + link;
+
+        if (!sitemap[link]) {
+          sitemap[link] = {};
+        }
+      }
+    }
+  }
+
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(body),
+          aTags = $('a');
+
+      var alternatesDom = $('link[rel="alternate"]');
+
+      if (canonical === '') {
+        canonical = $('link[rel="canonical"]')[0].attribs.href.slice(0, -1);
+      }
+
+      for (var a = 0; a < alternatesDom.length; a += 1) {
+        alternates[alternatesDom[a].attribs.href] = true;
+      }
+
+      getLinks(aTags, canonical, alternates);
+
+      var images = $('img'),
+          assets = [];
+      for (var f = 0; f < images.length; f += 1) {
+        assets.push(canonical + images[f].attribs.src);
+      }
+
+      sitemap[response.request.uri.href] = {};
+      sitemap[response.request.uri.href].assets = assets;
+      sitemap[response.request.uri.href].visited = true;
+
+      requestNextLink();
+
+    } else {
+      console.log('res: ++++ ', error);
+
+      requestNextLink();
+    }
+  }
+
+  getSitemap(sitemapUrl);
+};
---
+++ server.js
@@ -0,0 +1,43 @@
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express = require('express');        // call express
+var app = express();                 // define our app using express
+var bodyParser = require('body-parser');
+var sitemap = require('./app/controllers/sitemap/sitemap.controller');
+var timeout = require('connect-timeout');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8888;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function (req, res, next) {
+  // do logging
+  console.log('Something is happening.');
+  // Website you wish to allow to connect
+  //res.setHeader('Access-Control-Allow-Origin', 'http://dimpdigital.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next(); // make sure we go to the next routes and don't stop here
+});
+
+// more routes for our API will happen here
+router.route('/sitemap').get(sitemap.list);
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/', router);
+app.use(timeout(12000000));
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
